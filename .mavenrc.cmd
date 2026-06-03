2c318ebdd0c4303bc7a1a7aac523b76fc6e31afe
Lennart Abeln <98029406+lennartabeln@users.noreply.github.com>
2026-06-03 21:30:39 +0200

    Delete .mavenrc.cmd

--- .mavenrc.cmd
+++
@@ -1,2 +0,0 @@
-@REM Suppress sun.misc.Unsafe warnings from Maven's internal dependencies
-set MAVEN_OPTS=--add-opens java.base/sun.misc=ALL-UNNAMED -XX:+IgnoreUnrecognizedVMOptions -Dorg.slf4j.simpleLogger.defaultLogLevel=warn
